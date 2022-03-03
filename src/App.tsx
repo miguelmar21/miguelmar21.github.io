d4edb815622f6f49b108fef5a239f58f82d173e1
Miguel Regalado <miguelmobil21@gmail.com>
2022-03-03 14:15:44 -0600

    Planet testing - 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import * as React from 'react';
 import { hot } from "react-hot-loader/root";
 import Button from '@material-ui/core/Button';
 
-function App() {
+const App: React.FC = () => {
   return(
-      <div>
-        <h1>
-          Hello Miguel
-        </h1>
-        <Button variant="contained">this is a material UI button</Button>
+    <div>
+      <div className="planet"> 
+        <div className="wrap">
+          <div className="background"></div>
+          <div className="clouds"></div>   
+        </div>
+        <div className="mask"></div>
       </div>
+      <div className="planet-2"> 
+        <div className="wrap-2">
+          <div className="background-2"></div>
+          <div className="clouds-2"></div>   
+        </div>
+        <div className="mask-2"></div>
+      </div>
+    </div>
+    
     );
 }
 
-export default hot(App);
+export default App;
