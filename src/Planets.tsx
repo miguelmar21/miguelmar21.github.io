d88d95019ce8cb2f6ef3ab909c7a8877203f8718
Miguel Regalado <miguelmobil21@gmail.com>
2022-03-03 20:38:16 -0600

    Planet zoom on click

--- src/Planets.tsx
+++ src/Planets.tsx
@@ -1,24 +1,39 @@
-import * as React from 'react';
+import React, { useState } from "react";
+
+const clickedPlanetStlye={transform: "scale(3)", transition: "transform 2s"};
 
 const Planets: React.FC = () => {
+  const [clickedPlanet, setClickedPlanet] = useState("none");
+
+  const togglePlanetClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const className = (event.target as HTMLDivElement).className;
+    if (className === 'mask') {
+      setClickedPlanet('planet-1');
+    }
+  };
+
   return (
     <div>
-      <div className="planet"> 
+      <div
+        className="planet"
+        style={clickedPlanet === 'planet-1' ? clickedPlanetStlye : {}}
+        onClick={togglePlanetClick}
+      >
         <div className="wrap">
           <div className="background"></div>
-          <div className="clouds"></div>   
+          <div className="clouds"></div>
         </div>
         <div className="mask"></div>
       </div>
-      <div className="planet-2"> 
+      <div className="planet-2">
         <div className="wrap-2">
           <div className="background-2"></div>
-          <div className="clouds-2"></div>   
+          <div className="clouds-2"></div>
         </div>
         <div className="mask-2"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Planets;
